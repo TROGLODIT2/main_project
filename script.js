527893cb03a53a19e691ef0122b8a8f8ea1125f2
Данил <danil.gir2@gmail.com>
2026-05-28 17:12:46 +0300

    Добавляет картинки играм

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+let links_divs = document.querySelectorAll('.game-link');
+let links_list = ['src/picture/fridayNight.PNG', 'src/picture/geometryDashUFO2D.PNG', 'src/picture/standoffCases.PNG', 'src/picture/standoffCases.PNG'];
 
+
+for (let i = 0; i < links_list.length; i ++) {
+    links_divs[i].style.backgroundImage = `url(${links_list[i]})`;
+}
