c2a25a9be99aca8338197de99ba902340c1ec3d6
shkolnik1234 <danil.gir2@gmail.com>
2026-05-31 22:59:57 +0300

    Добавляет шашки на доску

--- shashki.js
+++ shashki.js
@@ -1,16 +1,16 @@
-// Тут добавляются клетки доски в родительский див
+// Тут добавляются клетки доски в грид
 let board = document.querySelector('.board');
 let color = 'white';
 
-for (let i = 1; i < 65; i ++) {
+for (let i = 1; i < 65; i++) {
     let cell = document.createElement('div');
-    cell.classList.add('cell');
+    
     if (color == 'white') {
         cell.setAttribute('class', 'white');
     } else {
         cell.setAttribute('class', 'black');
     }
-    cell.setAttribute('id', `${i}`);
+    cell.classList.add('cell');
     board.appendChild(cell);
     if (i % 8 != 0) {
         if (color == 'white') {
@@ -21,3 +21,64 @@ for (let i = 1; i < 65; i ++) {
     }
 }
 
+// Добавление всех клеток в список
+let cells = document.querySelectorAll('.cell');
+
+console.log(cells);
+
+// Добавление номеров клеток в список
+let cells_list = [];
+
+for (let i = 0; i < cells.length; i++) {
+    cells_list.push(i+1);
+}
+
+// Добавление названий клеток в список
+let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+let cells_names = [];
+
+for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+        cells_names.push(`${letters[j]}${i}`);
+    }
+}
+
+// Добавление белых шашек на начальные позиции
+let init_position = 40
+let cell_init_mode = 1;
+
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 8; j += 2) {
+        let checker = document.createElement('div');
+        checker.classList.add('checker_white');
+        checker.classList.add(`${i}`);
+        cells[init_position + i*8 + j].appendChild(checker);
+    }
+    if (cell_init_mode == 1) {
+        init_position += 1;
+        cell_init_mode = 2;
+    } else {
+        init_position -= 1;
+        cell_init_mode = 1;
+    }
+}
+
+// Добавление черных шашек на начальные позиции
+init_position = 1
+cell_init_mode = 1;
+
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 8; j += 2) {
+        let checker = document.createElement('div');
+        checker.classList.add('checker_black');
+        checker.classList.add(`${i}`);
+        cells[init_position + i*8 + j].appendChild(checker);
+    }
+    if (cell_init_mode == 1) {
+        init_position -= 1;
+        cell_init_mode = 2;
+    } else {
+        init_position += 1;
+        cell_init_mode = 1;
+    }
+}
