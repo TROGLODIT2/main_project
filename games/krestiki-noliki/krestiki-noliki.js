a3cd9545ff56211767a1c73d974cd6b4bb4eba1a
TROGLODIT2 <vasekamanov_13@mail.ru>
2026-06-08 21:28:56 +0300

    добавляет картинки крестика и нолика и временно ставится только крестик

--- games/krestiki-noliki/krestiki-noliki.js
+++ games/krestiki-noliki/krestiki-noliki.js
@@ -6,8 +6,16 @@ for (let i = 1; i < 10; i++) {
     cell.setAttribute('id', `${i}`);
     cell.classList.add('cell');
     board.appendChild(cell);
-    console.log('2222245ERHTYRGHTRF');
+    console.log('111');
 }
 
 // Добавление всех клеток в список
 let cells = document.querySelectorAll('.cell');
+
+
+for (let i = 0; i < cells.length; i++) {
+    console.log(cells[i])
+    cells[i].addEventListener('click', function () {
+        cells[i].style.backroundImage = 'url(https://troglodit2.github.io/main_project/src/picture/krest.png)';
+    })
+}
