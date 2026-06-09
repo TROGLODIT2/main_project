9ef411263d632f00366cb801441ccf8ad1c2df37
TROGLODIT2 <vasekamanov_13@mail.ru>
2026-06-09 20:35:43 +0300

    Обновляет krestiki-noliki.js

--- games/krestiki-noliki/krestiki-noliki.js
+++ games/krestiki-noliki/krestiki-noliki.js
@@ -6,7 +6,51 @@ for (let i = 1; i < 10; i++) {
     cell.setAttribute('id', `${i}`);
     cell.classList.add('cell');
     board.appendChild(cell);
+    console.log('111');
 }
 
 // Добавление всех клеток в список
 let cells = document.querySelectorAll('.cell');
+
+let move = 1;
+
+
+
+function getColor(index) {
+    console.log(cells[index].style.backgroundImage == 'url(https://troglodit2.github.io/main_project/src/picture/krest.png)')
+    if (cells[index].style.backgroundImage == 'url(https://troglodit2.github.io/main_project/src/picture/krest.png)') {
+        return 'krest';
+    }
+    if (cells[index].style.backgroundImage == 'url(https://troglodit2.github.io/main_project/src/picture/nolik.png)') {
+        return 'nolik'; 
+    }
+    return 'null';
+}
+
+
+
+for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener('click', function () {
+        if (cells[i].style.backgroundImage == '') {
+            if (move == 1) {
+                cells[i].style.backgroundImage = 'url(https://troglodit2.github.io/main_project/src/picture/krest.png)';
+                cells[i].style.backgroundPosition = 'center';
+                cells[i].style.backgroundRepeat = 'no-repeat';
+                cells[i].style.backgroundSize = 'contain';
+                move = 2
+            } else {
+                cells[i].style.backgroundImage = 'url(https://troglodit2.github.io/main_project/src/picture/nolik.png)';
+                cells[i].style.backgroundPosition = 'center';
+                cells[i].style.backgroundRepeat = 'no-repeat';
+                cells[i].style.backgroundSize = 'contain';
+                move = 1
+            }
+        }
+        console.log(getColor(0))
+
+        if (getColor(0) == 'krest' && getColor(1) == 'krest' && getColor(2) == 'krest') {
+            console.log('крестики пообедили');
+        }
+    })
+    
+}
