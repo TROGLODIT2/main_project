79e55b88e734cfcf59e73e3a4127f5767a9fb6db
TROGLODIT2 <vasekamanov_13@mail.ru>
2026-06-11 21:35:09 +0300

    Revert "Merge branch 'main' of https://github.com/TROGLODIT2/main_project"
    This reverts commit 9a6b4892d967e87e80334180b5714a291c646eae, reversing
    changes made to 17676a7950b782002a90cc886f8f0d15e023e4e0.

--- shashki.js
+++ shashki.js
@@ -1,17 +1,3 @@
-// Адаптивный навбар
-let open_menu = false;
-let open_menu_button = document.querySelector('.open_menu');
-
-open_menu_button.addEventListener('click', function () {
-    if (open_menu == false) {
-        document.querySelector('.menu_items').style.display = 'block';
-        open_menu = true;
-    } else {
-        document.querySelector('.menu_items').style.display = 'none';
-        open_menu = false;
-    }
-})
-
 // Тут добавляются клетки доски в грид
 let board = document.querySelector('.board');
 let color = 'white';
