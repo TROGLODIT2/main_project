a18c558c0b012574aee5c4b3dc46a243b0521ccd
TROGLODIT2 <vasekamanov_13@mail.ru>
2026-06-11 22:29:13 +0300

    добавляет поддрержку телефонов для крестиков-ноликов

--- games/krestiki-noliki/krestiki-noliki.js
+++ games/krestiki-noliki/krestiki-noliki.js
@@ -186,3 +186,16 @@ for (let i = 0; i < cells.length; i++) {
     })
     
 }
+
+let open_menu = false;
+let open_menu_button = document.querySelector('.open_menu');
+
+open_menu_button.addEventListener('click', function () {
+    if (open_menu == false) {
+        document.querySelector('.menu_items').style.display = 'block';
+        open_menu = true;
+    } else {
+        document.querySelector('.menu_items').style.display = 'none';
+        open_menu = false;
+    }
+})
