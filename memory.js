0b1f4f6d4655d4d141d98f900e11bb6741dfe738
shkolnik1234 <danil.gir2@gmail.com>
2026-06-13 08:00:46 +0300

    Инициализирует файлы мемори

--- memory.js
+++ memory.js
@@ -1,1 +1,21 @@
+// Открытие бургер меню
+let links = document.querySelectorAll('.game-link');
+let links_list = ['shashki_game.html', 'games/krestiki-noliki/krestiki-noliki.html'];
 
+for (let i = 0; i < game_bakground.length; i ++) {
+    links[i].href = `${links_list[i]}`;
+    links[i].style.display = 'block';
+}
+
+let open_menu = false;
+let open_menu_button = document.querySelector('.open_menu');
+
+open_menu_button.addEventListener('click', function () {
+    if (open_menu == false) {
+        document.querySelector('.menu_items').style.display = 'block';
+        open_menu = true;
+    } else {
+        document.querySelector('.menu_items').style.display = 'none';
+        open_menu = false;
+    }
+})
